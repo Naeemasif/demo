0715ea70b08b8a74651daf660644bb00651293be
naeem asif <naeemgoraya@yahoo.com>
2013-10-07 18:20:55 +0500

    devise gem added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+	
+	def index
+
+		@time = Time.now
+
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '378594631e1b179140a0729f200aad82b58ad36a812d5cee0aed77bab25e36574b0916c13df89b1582b3a542f4362d62af721160aa1981d68fe444724b702904'
