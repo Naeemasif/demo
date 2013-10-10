aa0cf52eb0406b8a1c802796dd822023cfaeb121
naeem asif <naeemgoraya@yahoo.com>
2013-10-10 19:32:02 +0500

    task E has been completed

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,16 @@ class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
   default_scope :order => 'title'
  # validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,:message => 'must be a URL for GIF, JPG or PNG image.'}
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+ # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors.add(:base, 'Line Items present' )
+      return false
+    end
+  end
+
 end
