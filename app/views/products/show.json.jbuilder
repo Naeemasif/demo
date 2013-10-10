3e8b7db31a60bfbcf3d2cfe9fdaf0b3c867edd4e
naeem asif <naeemgoraya@yahoo.com>
2013-10-10 14:51:54 +0500

    bootstrap is in place+cart application task A completed

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :title, :discription, :image_url, :price, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008050334) do
+ActiveRecord::Schema.define(version: 20131010061456) do
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "discription"
+    t.string   "image_url"
+    t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
